9ba374e593a91da65a057aeb9376bcfda6ff723b
Kamil Wozniak <kk-wozniak@outlook.com>
2025-04-09 20:45:36 +0100

    Finish D5 part 1

--- 2024/d5_print_queue/src/main.rs
+++ 2024/d5_print_queue/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    let pages_to_produce: Vec<Vec<u32>> = std::fs::read_to_string("./src/pages_to_produce")
+    let pages_to_produce: Vec<Vec<u32>> = std::fs::read_to_string("./src/pages_to_produce.txt")
         .unwrap()
         .lines()
         .map(|line| {
@@ -27,4 +27,59 @@ fn main() {
                 acc.entry(second).or_default().push(first);
                 acc
             });
+
+    println!(
+        "Middle value sum of valid pages: {}",
+        filter_pages(&pages_to_produce, &page_ordering_rules)
+    );
+}
+
+fn filter_pages(pages_to_produce: &[Vec<u32>], ordering_rules: &HashMap<u32, Vec<u32>>) -> u32 {
+    pages_to_produce
+        .iter()
+        .filter(|page| validate(page, ordering_rules))
+        .map(|page| page[page.len() / 2])
+        .sum()
+}
+
+fn validate(page: &[u32], ordering_rules: &HashMap<u32, Vec<u32>>) -> bool {
+    for i in 0..page.len() {
+        let empty_vec = vec![];
+        let v = ordering_rules.get(&page[i]).unwrap_or(&empty_vec);
+
+        if page[i..].iter().any(|&value| v.contains(&value)) {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid1() {
+        let pages_to_produce: Vec<Vec<u32>> = vec![
+            vec![75, 47, 61, 53, 29],
+            vec![97, 61, 53, 29, 13],
+            vec![75, 29, 13],
+            vec![75, 97, 47, 61, 53],
+            vec![61, 13, 29],
+            vec![97, 13, 75, 29, 47],
+        ];
+
+        let page_ordering_rules: HashMap<u32, Vec<u32>> = vec![
+            (13, vec![53, 75, 47, 29, 61, 97]),
+            (29, vec![47, 61, 53, 97, 75]),
+            (53, vec![97, 61, 75, 47]),
+            (61, vec![75, 47, 97]),
+            (75, vec![97]),
+        ]
+        .into_iter()
+        .collect();
+
+        assert_eq!(143, filter_pages(&pages_to_produce, &page_ordering_rules));
+    }
 }
